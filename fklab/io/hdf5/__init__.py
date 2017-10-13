b543b9828821b29a32e3f732b5548bf1f98b3e0c
Fabian Kloosterman <fabian.kloosterman@nerf.be>
2017-10-13 16:10:48 +0200

    Added hdf5 utility module.

---
+++ fklab/io/hdf5/__init__.py
@@ -0,0 +1,16 @@
+"""
+=====================================
+HDF5 utilities (:mod:`fklab.io.hdf5`)
+=====================================
+
+.. currentmodule:: fklab.io.hdf5
+
+Neuralynx file import functions.
+
+.. automodule:: fklab.io.hdf5.utilities
+    
+"""
+
+from .utilities import *
+
+__all__ = [s for s in dir() if not s.startswith('_')]
---
+++ fklab/io/hdf5/utilities.py
@@ -0,0 +1,26 @@
+import h5py
+import collections
+
+def map_to_hdf5(fid, data):
+    """Recursively save data in a map to hdf5 file.
+    
+    Parameters
+    ----------
+    fid : hdf5 file node
+    data :
+        any data object that can be saved to hdf5 file datasets. Dictionaries
+        will be traversed and keys are converted to hdf5 groups.
+    
+    """
+    
+    for k,v in data.items():
+        if isinstance(v, collections.Mapping):
+            group = fid.create_group(k)
+            map_to_hdf5(group, v)
+        else:
+            try:
+                fid[k] = v
+            except:
+                # warning or error?
+                pass
+
