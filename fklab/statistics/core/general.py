04b02435dbcb7a3c7e6a6b8c32ad63c87a817b35
Fabian Kloosterman <kloosterman.nerf@gmail.com>
2019-04-28 23:18:52 +0200

    Added functions to find mode of distribution and to reparameterize beta distribution.

--- fklab/statistics/core/general.py
+++ fklab/statistics/core/general.py
@@ -11,12 +11,16 @@ General utilities.
     :toctree: generated/
     
     monte_carlo_pvalue
+    find_mode
+    beta_reparameterize
 
 """
 
-__all__ = ['monte_carlo_pvalue']
+__all__ = ['monte_carlo_pvalue', 'find_mode', 'beta_reparameterize']
 
 import numpy as np
+import scipy as sp
+import scipy.stats
 
 def monte_carlo_pvalue(simulated, test, tails='right', center=0, axis=0):
     """Compute Monte Carlo p-value.
@@ -76,3 +80,64 @@ def monte_carlo_pvalue(simulated, test, tails='right', center=0, axis=0):
     
     return p
 
+def find_mode(x, axis=0, gridsize=201):
+    """Find mode in observations
+    
+    To determine the mode of a set of observations, a kernel
+    density estimate (kde) of the distribution is computed and
+    evaluated at a grid. The mode is then taken as the point at
+    which the kde is maximal.
+
+    Parameters
+    ----------
+    x : array
+    axis : int
+        axis along which to find the mode
+    gridsize : int
+        number of grid points to compute kernel density estimate
+    
+    Returns
+    -------
+    mode : array
+    
+    """
+    
+    shape = x.shape
+    ndim = x.ndim
+    
+    # reshape
+    x = np.moveaxis(x, axis, -1)
+    new_shape = x.shape[:-1] if ndim>1 else (1,)
+    x = np.reshape(x, (x.size//shape[axis], shape[axis]))
+    
+    l,u = np.min(x, axis=1), np.max(x, axis=1)
+    
+    mode = np.full(x.shape[0], np.nan)
+    
+    for k,y in enumerate(x):
+        grid = np.linspace(l[k], u[k], gridsize)
+        density = sp.stats.gaussian_kde(y)(grid)
+        mode[k] = grid[np.argmax(density)]
+    
+    mode = np.reshape(mode, new_shape)
+    
+    if ndim==1:
+        mode = float(mode)
+    
+    return mode
+
+def beta_reparameterize(mode=0.5, concentration=4):
+    """Reparameterization of beta distribution
+    
+    Parameters
+    ----------
+    mode : float in range [0,1]
+    concentration : float >= 2
+    
+    Returns
+    -------
+    alpha, beta : float
+    
+    """
+    
+    return (mode*(concentration-2)+1, (1-mode)*(concentration-2)+1)
