2fc5d05d120418115fc1a948182e1c1fc38785db
Fabian Kloosterman <fabian.kloosterman@nerf.be>
2020-03-22 11:20:27 +0100

    Added new utilities and polar modules to __init__.py.

--- fklab/plot/plots/__init__.py
+++ fklab/plot/plots/__init__.py
@@ -8,9 +8,11 @@ Low-level plots (:mod:`fklab.plot.plots`)
 Low-level plots.
 
 .. automodule:: fklab.plot.plots.plots
-    
+
 """
-
 from .plots import *
+from .polar import *
+from .utilities import *
 
-__all__ = [s for s in dir() if not s.startswith('_')]
+
+__all__ = [s for s in dir() if not s.startswith("_")]
