c8510b5ad25b2be7daf3ec23f931577d8b498e65
Lies Deceuninck <lies.deceuninck@nerf.be>
2021-08-23 09:23:48 +0000

    add test with fill_value=np,nan

--- tests/test_basic_algorithm.py
+++ tests/test_basic_algorithm.py
@@ -22,7 +22,14 @@ class TestBasicAlgorithm(unittest.TestCase):
             self.signal, self.artefacts, time=self.time, interp="fill", fill_value=0
         )  # default interpolation is linear
         np.testing.assert_array_equal(result, expected)
-
+        
+    def test_remove_artefacts_fill(self):
+        expected = np.array([0, np.nan, np.nan, np.nan, 4, 5, 6, 7, 8, 9])
+        result = remove_artefacts(
+            self.signal, self.artefacts, time=self.time, interp="fill", fill_value=np.nan
+        )  # default interpolation is linear
+        np.testing.assert_array_equal(result, expected)
+        
     def test_remove_artefacts_interpolate_linear(self):
         signal = np.array([0, 1, 1.2, 1.3, 4, 5, 6, 7, 8, 9])
         expected = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
